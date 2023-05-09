225660bcabce34b6830d3e150d73b25cb93e2cd5
Hazel <hazelchavez56@gmail.com>
2023-05-09 14:34:53 -0600

    Cambiando modo oscuro

--- assets/js/btn-fill.js
+++ assets/js/btn-fill.js
@@ -1,6 +1,6 @@
 const btnFill = document.querySelector("#btn-fill");
 
 btnFill.addEventListener("click", () => {
-    document.body.classList.toggle('dark');//pone la clase dark y la quita
+    document.body.classList.toggle('light');//pone la clase dark y la quita
    btnFill.classList.toggle('active');
 })
