62238119b5d1740625eeb7a019489aeb770343a0
Hazel <hazelchavez56@gmail.com>
2023-05-29 16:59:53 -0600

    Realizando modificaciones en el logo

---
+++ assets/js/Switch.js
@@ -0,0 +1,6 @@
+const Switch = document.querySelector("#switch");
+
+Switch.addEventListener("click", () => {
+    document.body.classList.toggle('light');//pone la clase dark y la quita
+   Switch.classList.toggle('active');
+})
--- assets/js/btn-fill.js
+++
@@ -1,6 +0,0 @@
-const btnFill = document.querySelector("#btn-fill");
-
-btnFill.addEventListener("click", () => {
-    document.body.classList.toggle('light');//pone la clase dark y la quita
-   btnFill.classList.toggle('active');
-})
