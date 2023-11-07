5c00f6019599378ab799bccb3bb26f1f49943ffb
Hazel <hazelchavez56@gmail.com>
2023-11-07 15:42:26 -0600

    Quitando selector

--- assets/js/formSubmit.js
+++ assets/js/formSubmit.js
@@ -1,6 +1,42 @@
 
-function showCustomPopup() {
-	alert('Thanks for contacting me');
+// const form = document.getElementById("form")
+
+// form = (e) =>{
+// e.preventEventDefault
+// }
+// document.getElementById("form").addEventListener("submit", function(event) {
+//     // Evitar el envío predeterminado del formulario
+//     event.preventDefault();
+
+//     var inpust = document.querySelectorAll("input");
+
+//   inpust.forEach(function(input){
+//       input.value = "";
+//    })
+//    document.querySelector("textarea").value = ""
+// //    // Muestra el mensaje de "Mensaje enviado con éxito"
+//     document.getElementById("mensajeEnviado").style.display = "block";
+
+// //    // Puedes realizar aquí cualquier otra acción necesaria, como enviar el formulario al servidor.
+//  });
+
+// document.getElementById("form").addEventListener("submit", showMessaje,function(e) {
+//    e.preventDefault();
+   
+// })
+   
+
+
+
+ function showMessaje  () {
+    document.getElementById("form").addEventListener("submit", function(event) {
+        //     // Evitar el envío predeterminado del formulario
+            event.preventDefault();
+        
+   alert("Thanks for me");
+    })
 }
 
-document.getElementById('#submit').addEventListener('submit', showCustomPopup);
+
+
+
