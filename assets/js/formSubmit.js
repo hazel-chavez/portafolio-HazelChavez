53352e63e3718066ab2cc52b84f157814c62efd8
Hazel <hchaveza@ucenfotec.ac.cr>
2025-06-08 14:58:22 -0600

    add new features

--- assets/js/formSubmit.js
+++ assets/js/formSubmit.js
@@ -1,42 +1,20 @@
-
-// const form = document.getElementById("form")
-
-// form = (e) =>{
-// e.preventEventDefault
-// }
-// document.getElementById("form").addEventListener("submit", function(event) {
-//     // Evitar el envío predeterminado del formulario
-//     event.preventDefault();
-
-//     var inpust = document.querySelectorAll("input");
-
-//   inpust.forEach(function(input){
-//       input.value = "";
-//    })
-//    document.querySelector("textarea").value = ""
-// //    // Muestra el mensaje de "Mensaje enviado con éxito"
-//     document.getElementById("mensajeEnviado").style.display = "block";
-
-// //    // Puedes realizar aquí cualquier otra acción necesaria, como enviar el formulario al servidor.
-//  });
-
-// document.getElementById("form").addEventListener("submit", showMessaje,function(e) {
-//    e.preventDefault();
-   
-// })
-   
-
-
-
- function showMessaje  () {
-    document.getElementById("form").addEventListener("submit", function(event) {
-        //     // Evitar el envío predeterminado del formulario
-            event.preventDefault();
-        
-   alert("Thanks for me");
-    })
-}
-
-
-
-
+//wait until all the Html this content is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+	//create a varible and get the element  with id "form"
+	const form = document.getElementById('form');
+	//listen for the 'submit' on the form
+	form.addEventListener('submit', function (e) {
+		e.preventDefault(); // prevent the defaul form submission(stops automatic sending and page reload)
+		//show a success messaje using sweetAlert2
+		Swal.fire({
+			icon: 'success',
+			title: 'Messaje sending', //title of the alert
+			html: 'Thanks for <b>contacting</b> me.<br>¡I’ll <u>reply soon!</u>', //main messaje
+			draggable: true,
+			confirmButtonText: 'Got it', // text on the confirmation button
+			footer: '<span style="font-size: 12px;">Hazel Portfolio</span>',
+		}).then(() => {
+			form.submit(); // After the alert is closed, submit the form to formSubmit
+		});
+	});
+});
